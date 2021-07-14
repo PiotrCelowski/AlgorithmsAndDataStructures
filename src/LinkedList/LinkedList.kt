6e3802753a0f83b745ef4beaff1d35b873bcdfe4
Piotr Celowski <piotr.celowski@gmail.com>
2021-07-14 19:53:00 +0200

    LinkedList exercise

---
+++ src/LinkedList/LinkedList.kt
@@ -0,0 +1,59 @@
+package LinkedList
+
+
+class LinkedList<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
+    private var size = 0
+    fun isEmpty(): Boolean {
+        return size == 0
+    }
+
+    override fun toString(): String {
+        if (isEmpty()) {
+            return "Empty list"
+        } else {
+            return head.toString()
+        }
+    }
+
+    fun push(value: T): LinkedList<T> {
+        head = Node(value = value, next = head)
+        if (tail == null) {
+            tail = head
+        }
+        size++
+        return this
+    }
+
+    fun append(value: T) {
+        if (isEmpty()) {
+            push(value)
+            return
+        }
+        tail?.next = Node(value = value)
+        tail = tail?.next
+        size++
+    }
+
+    fun nodeAt(index: Int): Node<T>? {
+        var currentNode = head
+        var currentIndex = 0
+        while (currentNode != null && currentIndex < index) {
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        return currentNode
+    }
+
+    fun insert(value: T, afterNode: Node<T>): Node<T> {
+        if (tail == afterNode) {
+            append(value)
+            return tail!!
+        }
+        val newNode = Node(value = value, next = afterNode.next)
+        afterNode.next = newNode
+        size++
+        return newNode
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,15 @@
+import LinkedList.LinkedList
+import LinkedList.Node
+
 fun main() {
-    val seq = Sequencer()
-    val fibo = seq.calculateFibonacci(6)
-    print(fibo)
+    val list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    println("Before inserting: $list")
+    var middleNode = list.nodeAt(1)!!
+    for (i in 1..3) {
+        middleNode = list.insert(-1 * i, middleNode)
+    }
+    println("After inserting: $list")
 }
